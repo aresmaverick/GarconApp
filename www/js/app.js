5360b41033482b47efe9c38f3c57387165f2e054
Frederico de Oliveira <fredericoliveira@gmail.com>
2016-04-05 22:49:33 -0300

    Término do capitulo 5 do Alura.

--- www/js/app.js
+++ www/js/app.js
@@ -10,3 +10,26 @@ $('.collection').on('click', '.collection-item', function(){
 
   Materialize.toast(nomeProduto + ' adicionado.', 1000);
 });
+
+$('.modal-trigger').leanModal();
+
+$('#confirmar').on('click', function() {
+  var texto = "";
+
+  $('.badge').parent().each(function() {
+    texto += this.firstChild.textContent + ': ';
+    texto += this.lastChild.textContent + ', ';
+  });
+
+  $('#resumo').empty().text(texto);
+});
+
+$('.collection').on('click', '.badge', function() {
+  $(this).remove();
+  return false;
+});
+
+$('.acao-limpar').on('click', function() {
+  $('#numero-mesa').val('');
+  $('.badge').remove();
+});
